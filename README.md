1e2eb99a3b31c09940afb6ef03565262ab2d7877
Pedrozle <pedrozle@outlook.com>
2023-03-05 19:46:50 -0400

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# gerenciador-projetos
-Um projeto em javascript para auxiliar o usuário a genrenciar seus projetos pessoais
---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+
+
+import './App.css'
+
+const App = () => {
+    return ( 
+        <> <p> TODO </p>
+            <BrowserRouter>
+                <Routes>
+                   
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
+ 
+export default App;
